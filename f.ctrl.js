3f926209e753df857183ffa46fef2e35dc074065
= <kaven.feng@autodesk.com>
2015-05-02 06:55:00 +0800

    ng-submit

--- f.ctrl.js
+++ f.ctrl.js
@@ -45,5 +45,10 @@ angular.module('app').controller("MainController", function(){
     }
     ];
     vm.order = vm.orders[0];
-
+	
+	vm.new = {};
+	vm.addShow = function() {
+    	vm.shows.push(vm.new);
+    	vm.new = {};
+	};
 });
