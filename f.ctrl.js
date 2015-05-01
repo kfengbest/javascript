a3eaf93b4cdea020ca8ec3bb4ac2e1be08441355
= <kaven.feng@autodesk.com>
2015-05-02 06:38:10 +0800

    select, ng-options, filter by using order

--- f.ctrl.js
+++ f.ctrl.js
@@ -11,5 +11,38 @@ angular.module('app').controller("MainController", function(){
     	title: 'walking dead',
     	year: 2011,
     	favorite: false
+    },
+    {
+    	title: 'gold age',
+    	year: 2011,
+    	favorite: true
     }];
+
+    vm.orders = [
+    {
+    	id : 1,
+    	title : 'year asending',
+    	key : 'year',
+    	reverse : false
+    },
+    {
+    	id : 2,
+    	title : 'year desending',
+    	key : 'year',
+    	reverse : true
+    },
+    {
+    	id : 3,
+    	title : 'title asending',
+    	key : 'title',
+    	reverse : false
+    },
+    {
+    	id : 4,
+    	title : 'title desending',
+    	key : 'title',
+    	reverse : true
+    }
+    ];
+    vm.order = vm.orders[0];
 });
