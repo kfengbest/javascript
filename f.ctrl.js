1de599ed1563deed87b578cfa55dc3fe6fa29192
= <kaven.feng@autodesk.com>
2015-05-02 06:13:45 +0800

    using ng-repeat

--- f.ctrl.js
+++ f.ctrl.js
@@ -1,4 +1,14 @@
 angular.module('app').controller("MainController", function(){
     var vm = this;
     vm.title = 'AngularJS Tutorial Example';
+    vm.shows = [{
+    	title: 'game of thrones',
+    	year: 2001,
+    	favorite: true
+    },
+    {
+    	title: 'walking dead',
+    	year: 2011,
+    	favorite: false
+    }];
 });
