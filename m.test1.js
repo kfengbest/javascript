247023a579da78c11528efeb05560148ddea1af7
fengka <kaven.feng@autodesk.com>
2015-06-12 14:19:45 +0800

    more test on expect.js, equal, eql, ok

--- m.test1.js
+++ m.test1.js
@@ -5,3 +5,25 @@ describe('Array', function(){
     })
   })
 })
+
+describe('Expect.js', function(){
+	describe('ok', function(){
+		it('1 should be ok', function(){
+			expect(1).to.be.ok();
+		})
+		it('true should be ok', function(){
+			expect(true).to.be.ok();
+		})
+	});
+	describe('equal', function(){
+		it('asserts === equality', function(){
+			expect(1).to.be(1);
+		})
+
+	});
+	describe('eql', function(){
+		it('asserts loose equality that works with objects', function(){
+			expect({a:'b'}).to.eql({a:'b'});
+		});
+	});
+})
